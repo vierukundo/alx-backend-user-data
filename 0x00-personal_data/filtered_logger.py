04a3780b9862ab181141f07e509718159116bf28
vierukundo <vierukundo@gmail.com>
2024-02-11 22:56:35 +0000

    Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -58,7 +58,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """
     Connect to the MySQL database and return a connector.
     """
