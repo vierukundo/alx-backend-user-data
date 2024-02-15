73d807021d7960680715fb0b24ff6aff138894bd
vierukundo <vierukundo@gmail.com>
2024-02-15 19:38:21 +0000

    Updated @app.before_request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,9 +32,12 @@ def filter_request():
         excluded_paths = [
                 '/api/v1/status/',
                 '/api/v1/unauthorized/',
-                '/api/v1/forbidden/']
+                '/api/v1/forbidden/',
+                '/api/v1/auth_session/login/']
         if auth.require_auth(request.path, excluded_paths):
             if not auth.authorization_header(request):
+                return abort(401)
+            if not auth.session_cookie(request):
                 return abort(401)
             if not auth.current_user(request):
                 return abort(403)
