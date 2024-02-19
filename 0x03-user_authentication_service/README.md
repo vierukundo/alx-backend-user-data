c1130e6b8737d7401c446c4c76126105bbf909f1
vierukundo <vierukundo@gmail.com>
2024-02-19 22:39:12 +0000

    Completed DB class

---
+++ 0x03-user_authentication_service/README.md
@@ -0,0 +1,7 @@
+# 0x03. User authentication service
+This project helps to understand more these concepts:
+
+How to declare API routes in a Flask app
+How to get and set cookies
+How to retrieve request form data
+How to return various HTTP status codes
---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""Module for DB class"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from user import Base
+from user import User
+from typing import TypeVar, Any, Dict
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """adds user to database"""
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs: Any) -> User:
+        """takes in any keyword arguments and returns the first row found
+        in the users table as filtered by the method’s input arguments."""
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise NoResultFound()
+        except InvalidRequestError:
+            raise InvalidRequestError()
+        return user
