4907e6a07d8c888da84708cfa698faeb4dca456c
vierukundo <vierukundo@gmail.com>
2024-02-20 22:56:26 +0000

    Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -51,11 +51,10 @@ def logout():
     """Endpoint to handle user logout."""
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
-    if user:
-        AUTH.destroy_session(user.id)
-        return redirect("/")
-    else:
+    if not user:
         abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect("/")
 
 
 if __name__ == "__main__":
