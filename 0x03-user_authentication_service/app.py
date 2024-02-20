f1f3bc5e37cefa6c3fb1e86c9419bf11dabe49e3
vierukundo <vierukundo@gmail.com>
2024-02-20 22:59:41 +0000

    Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -46,15 +46,15 @@ def login():
         abort(401)
 
 
-@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
-def logout():
-    """Endpoint to handle user logout."""
-    session_id = request.cookies.get('session_id')
+@app.route("/sessions", methods=["DELETE"])
+def logout() -> str:
+    """Handle a DELETE request to log out a user."""
+    session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
     if not user:
         abort(403)
     AUTH.destroy_session(user.id)
-    return redirect("/")
+    return redirect('/')
 
 
 if __name__ == "__main__":
