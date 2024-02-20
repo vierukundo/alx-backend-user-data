c02a89d6a0ede9c42b69b572df685aaad56c0c2c
vierukundo <vierukundo@gmail.com>
2024-02-20 18:29:03 +0000

    set echo flag value to False

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,7 +32,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar(User):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """adds user to database"""
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
